bc4472607942106a0649bc841b31202e363e6831
Dan Bonachea <DOBonachea@lbl.gov>
2002-10-27 11:30:39 +0000

    relax semantics of GASNET_BEGIN_FUNCTION slight relaxation of the GASNET_BEGIN_FUNCTION semantics allows an implementation which lazily postpones thread discovery to the first actual use (ensuring that GASNET_BEGIN_FUNCTION has no runtime cost in functions which don't actually use GASNet calls)

--- elan-conduit/gasnet_extended_fwd.h
+++ elan-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
-/*  $Archive:: /Ti/GASNet/elan-conduit/gasnet_extended_fwd.h                  $
- *     $Date: 2002/09/02 23:18:37 $
- * $Revision: 1.2 $
+/*  $Archive:: /Ti/GASNet/extended/gasnet_extended_fwd.h                  $
+ *     $Date: 2002/10/27 11:30:36 $
+ * $Revision: 1.3 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  */
@@ -18,8 +18,15 @@
 #define GASNET_EXTENDED_NAME_STR     _STRINGIFY(GASNET_EXTENDED_NAME)
 
 
+#define _GASNET_HANDLE_T
+/*  an opaque type representing a non-blocking operation in-progress initiated using the extended API */
+struct _gasnete_op_t;
+typedef struct _gasnete_op_t *gasnet_handle_t;
+#define GASNET_INVALID_HANDLE ((gasnet_handle_t)0)
+
 #ifdef GASNETI_THREADS
   #define GASNETI_THREADINFO_OPT
+  #define GASNETI_LAZY_BEGINFUNCTION
 #endif
 
 #ifdef GASNETI_THREADINFO_OPT
@@ -28,7 +35,7 @@
       of if it bound to the globally declared dummy variables. 
      Even a very stupid C optimizer should constant-fold away the unused calls to gasneti_get_threadinfo() 
       and discard the unused variables
-     We need 2 separate variables to ensure correct name-binding semantics for GASNET_BEGIN_FUNCTION()
+     We need 2 separate variables to ensure correct name-binding semantics for GASNET_POST_THREADINFO(GASNET_GET_THREADINFO())
    */
   static uint8_t gasnete_threadinfo_cache = 0;
   static uint8_t gasnete_threadinfo_available = 
@@ -41,22 +48,40 @@
     /* if you get an unused variable warning on gasnete_threadinfo_available, 
        it means you POST'ed in a function which made no GASNet calls that needed it */
 
-  #define GASNET_GET_THREADINFO()                  \
-    ( (sizeof(gasnete_threadinfo_available) == 1) ? \
-      (gasnet_threadinfo_t)gasnete_mythread() :    \
-      (gasnet_threadinfo_t)(uintptr_t)gasnete_threadinfo_cache )
+  #ifdef GASNETI_LAZY_BEGINFUNCTION
+    #define GASNET_GET_THREADINFO()                                   \
+      ( (sizeof(gasnete_threadinfo_available) == 1) ?                 \
+        (gasnet_threadinfo_t)gasnete_mythread() :                     \
+        ( (uintptr_t)gasnete_threadinfo_cache == 0 ?               \
+          ((gasnet_threadinfo_t)(uintptr_t)gasnete_threadinfo_cache = \
+            (gasnet_threadinfo_t)gasnete_mythread()) :                \
+        (gasnet_threadinfo_t)(uintptr_t)gasnete_threadinfo_cache) )
+  #else
+    #define GASNET_GET_THREADINFO()                   \
+      ( (sizeof(gasnete_threadinfo_available) == 1) ? \
+        (gasnet_threadinfo_t)gasnete_mythread() :     \
+        (gasnet_threadinfo_t)(uintptr_t)gasnete_threadinfo_cache )
+  #endif
 
   /* the gasnet_threadinfo_t pointer points to a thread data-structure owned by
      the extended API, whose first element is a pointer reserved
      for use by the core API (initialized to NULL)
    */
+
+  #ifdef GASNETI_LAZY_BEGINFUNCTION
+    /* postpone thread discovery to first use */
+    #define GASNET_BEGIN_FUNCTION() GASNET_POST_THREADINFO(0)
+  #else
+    #define GASNET_BEGIN_FUNCTION() GASNET_POST_THREADINFO(GASNET_GET_THREADINFO())
+  #endif
+
 #else
   #define GASNET_POST_THREADINFO(info)   \
     static uint8_t gasnete_dummy = sizeof(gasnete_dummy) /* prevent a parse error */
   #define GASNET_GET_THREADINFO() (NULL)
+  #define GASNET_BEGIN_FUNCTION() GASNET_POST_THREADINFO(GASNET_GET_THREADINFO())
 #endif
 
-#define GASNET_BEGIN_FUNCTION() GASNET_POST_THREADINFO(GASNET_GET_THREADINFO())
 
   /* this can be used to add statistical collection values 
      specific to the extended API implementation (see gasnet_help.h) */
