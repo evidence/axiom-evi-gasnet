ac6986b44ac066ad0500b50d4988a6e476addda9
Paul H. Hargrove <PHHargrove@lbl.gov>
2004-03-03 17:58:41 +0000

    Extend testbarrier to verify behavior on mismatched named barriers, etc.

--- tests/testbarrier.c
+++ tests/testbarrier.c
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/tests/testbarrier.c                             $
- *     $Date: 2002/12/19 18:31:54 $
- * $Revision: 1.5 $
+ *     $Date: 2004/03/03 17:58:41 $
+ * $Revision: 1.6 $
  * Description: GASNet barrier performance test
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -13,6 +13,7 @@
 int main(int argc, char **argv) {
   int mynode, iters=0;
   int64_t start,total;
+  int result;
   int i = 0;
 
   GASNET_Safe(gasnet_init(&argc, &argv));
@@ -60,6 +61,52 @@ int main(int argc, char **argv) {
         ((float)total)/1000000, ((float)total)/iters);
       fflush(stdout);
   }
+
+  BARRIER();
+
+  /* Test for required failures: */
+
+  /* node 0 indicates mismatch on entry: */
+  gasnet_barrier_notify(0, !mynode ? GASNET_BARRIERFLAG_MISMATCH : 0);
+  result = gasnet_barrier_wait(0, !mynode ? GASNET_BARRIERFLAG_MISMATCH : 0);
+  if (result != GASNET_ERR_BARRIER_MISMATCH) {
+    MSG("Failed to detect barrier mismatch indicated on notify.");
+    gasnet_exit(1);
+  }
+
+  /* ids differ between notify and wait */
+  gasnet_barrier_notify(0, 0);
+  result = gasnet_barrier_wait(1, 0);
+  if (result != GASNET_ERR_BARRIER_MISMATCH) {
+    MSG("Failed to detect mismatch between id at notify and wait.");
+    gasnet_exit(1);
+  }
+
+  /* Flags differ between notify and wait: */
+  gasnet_barrier_notify(0, GASNET_BARRIERFLAG_ANONYMOUS);
+  result = gasnet_barrier_wait(0, 0);
+  if (result != GASNET_ERR_BARRIER_MISMATCH) {
+    MSG("Failed to detect anonymous notify with named wait.");
+    gasnet_exit(1);
+  }
+  gasnet_barrier_notify(0, 0);
+  result = gasnet_barrier_wait(0, GASNET_BARRIERFLAG_ANONYMOUS);
+  if (result != GASNET_ERR_BARRIER_MISMATCH) {
+    MSG("Failed to detect named notify with anonymous wait.");
+    gasnet_exit(1);
+  }
+
+  /* Mismatched id: */
+  if (gasnet_nodes() > 1) {
+    gasnet_barrier_notify(!mynode, 0);
+    result = gasnet_barrier_wait(0, 0);
+    if (result != GASNET_ERR_BARRIER_MISMATCH) {
+      MSG("Failed to detect different id on node 0.");
+      gasnet_exit(1);
+    }
+  }
+
+  BARRIER();
 
   MSG("done.");
 
