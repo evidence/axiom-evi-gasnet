2eddd93c808232ee2859d7bccf84bf81d5002ebc
Dan Bonachea <DOBonachea@lbl.gov>
2004-06-20 08:47:59 +0000

    Add a new gasnet test testcxx It's a simple C++ wrapper around testgasnet, and tests the ability for a C++-based GASNet client to successfully compile the GASNet headers and link the GASNet library. The test is only enabled if a C++ compiler was detected at configure time (ie udp-conduit is enabled).

---
+++ tests/testcxx.cc
@@ -0,0 +1,9 @@
+/*  $Archive:: /Ti/GASNet/tests/testgasnet.c                              $
+ *     $Date: 2004/06/20 08:47:59 $
+ * $Revision: 1.1 $
+ * Description: General GASNet correctness tests in C++
+ * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
+ * Terms of use are as specified in license.txt
+ */
+#include "testgasnet.c"
+
