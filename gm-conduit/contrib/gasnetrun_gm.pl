27b945d2d75f8cfffcf7bd23890dba3b8b7a7615
Christian Bell <csbell@cs.berkeley.edu>
2004-08-19 23:03:11 +0000

    Fix jason's doc update
    we have GASNET_MACHINEFILE env var for non-pbs environments which prefer to use
    a machinefile.

--- gm-conduit/contrib/gasnetrun_gm.pl
+++ gm-conduit/contrib/gasnetrun_gm.pl
@@ -410,7 +410,7 @@ $machine_file = $default_machinefile unless defined ($machine_file);
 
 if (!$machine_file && ($rexec_type ne "gexec")) {
     printf "Can't detect a PBS or a GEXEC environment.  If you are not running\n"
-         . "within a batch system, set the PBS_NODEFILE environment variable to\n"
+         . "within a batch system, set the GASNET_NODEFILE environment variable to\n"
          . "a file containing one hostname per line (the first process will use \n"
          . "the host on the first line, etc.)\n";
     exit 1;
