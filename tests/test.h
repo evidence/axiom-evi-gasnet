e4d69e02328af175513c79f8598751a7db43076b
Dan Bonachea <DOBonachea@lbl.gov>
2006-01-21 20:25:29 +0000

    add hostname indicators to gasnet tests header CVSMAIL: CC:

--- tests/test.h
+++ tests/test.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/tests/test.h,v $
- *     $Date: 2005/10/25 07:14:00 $
- * $Revision: 1.63 $
+ *     $Date: 2006/01/21 20:25:29 $
+ * $Revision: 1.64 $
  * Description: helpers for GASNet tests
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -709,8 +709,12 @@ static void _test_init(const char *testname, int reports_performance, int early)
     MSG0("=====> %s nprocs=%d config=%s",
         testname, (int)gasnet_nodes(), GASNET_CONFIG_STRING);
     if (!early) {
+      char hostname[255];
       TEST_SEG(gasnet_mynode()); /* ensure we got the segment requested */
       BARRIER();
+      if (!gethostname(hostname,255)) {
+        MSG("hostname is: %s", hostname);
+      }
     }
   #else
     MSG0("=====> %s config=%s",
