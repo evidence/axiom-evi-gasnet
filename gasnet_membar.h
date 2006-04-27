c8dfafba49b031b8b43be2e13697ebc3a6847d49
Paul H. Hargrove <PHHargrove@lbl.gov>
2006-04-27 23:34:46 +0000

    Move GASNETI_ASM() out of gasnet_membar.h, creating gasnet_asm.h. This eliminates the ugly hack of including only part of gasnet_membar.h from configure. File cloned in CVS to keep history.

--- gasnet_membar.h
+++ gasnet_membar.h
@@ -1,12 +1,12 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/gasnet_membar.h,v $
- *     $Date: 2006/04/19 20:28:45 $
- * $Revision: 1.103 $
+ *     $Date: 2006/04/27 23:34:46 $
+ * $Revision: 1.104 $
  * Description: GASNet header for portable memory barrier operations
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
  */
 
-#if !defined(_IN_GASNET_TOOLS_H) && !defined(_IN_GASNET_H) && !defined(_IN_CONFIGURE)
+#if !defined(_IN_GASNET_TOOLS_H) && !defined(_IN_GASNET_H)
   #error This file is not meant to be included directly- clients should include gasnet.h or gasnet_tools.h
 #endif
 
@@ -64,43 +64,7 @@
 
   For more info on memory barriers: http://gee.cs.oswego.edu/dl/jmm/cookbook.html
  */
-#if defined(__GNUC__)
-  #define GASNETI_ASM(mnemonic) __asm__ __volatile__ (mnemonic : : : "memory")
-#elif defined(__INTEL_COMPILER)
-  #define GASNETI_ASM(mnemonic) __asm__ __volatile__ (mnemonic : : : "memory")
-#elif defined(PGI_WITH_REAL_ASM)
-  #define GASNETI_ASM(mnemonic) __asm__ __volatile__ (mnemonic : : : "memory")
-#elif defined(__PGI) /* note this requires compiler flag -Masmkeyword */
-  #define GASNETI_ASM(mnemonic) asm(mnemonic)
-#elif defined(__DECC) || defined(__DECCXX)
-  #include <c_asm.h>
-  #define GASNETI_ASM(mnemonic) asm(mnemonic)
-#elif defined(_SGI_COMPILER_VERSION) /* MIPSPro C */
-  #define GASNETI_ASM(mnemonic)  ERROR_NO_INLINE_ASSEMBLY_AVAIL /* not supported or used */
-#elif defined(__SUNPRO_C) /* Sun C works, Sun C++ lacks inline assembly support (man inline) */
-  #define GASNETI_ASM(mnemonic)  __asm(mnemonic)
-#elif defined(_SX)  
-  #define GASNETI_ASM(mnemonic)  asm(mnemonic)
-#elif defined(__HP_cc) /* HP C */
-  #define GASNETI_ASM(mnemonic)  _asm(mnemonic)
-#elif defined(__HP_aCC)
-  #define GASNETI_ASM(mnemonic)  ERROR_NO_INLINE_ASSEMBLY_AVAIL /* not supported or used */
-#elif defined(__SUNPRO_CC)
-  #define GASNETI_ASM(mnemonic)  ERROR_NO_INLINE_ASSEMBLY_AVAIL /* not supported or used */
-#elif defined(__xlC__)  
-  #define GASNETI_ASM(mnemonic)  ERROR_NO_INLINE_ASSEMBLY_AVAIL /* not supported or used */
-#elif defined(_CRAY)  
-  #define GASNETI_ASM(mnemonic)  ERROR_NO_INLINE_ASSEMBLY_AVAIL /* not supported or used */
-#elif defined(__MTA__)  
-  #define GASNETI_ASM(mnemonic)  ERROR_NO_INLINE_ASSEMBLY_AVAIL /* not supported or used */
-#else
-  #error "Don't know how to use inline assembly for your compiler"
-#endif
-
-
-#ifdef _IN_CONFIGURE
-  /* the file effectively ends here */
-#else
+#include <gasnet_asm.h>
 
 #if defined(__SUNPRO_CC) || defined(__HP_aCC) || (defined(PGI_WITH_REAL_ASM) && defined(__cplusplus))
   /* no inline assembly in these C++ compilers, so pay a function call overhead */
@@ -569,7 +533,5 @@
 #endif
 
 /* ------------------------------------------------------------------------------------ */
-
-#endif
 
 #endif
