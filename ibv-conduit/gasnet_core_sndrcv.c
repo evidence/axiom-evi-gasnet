1b51f2b9ecd631ce3b53f95e927d46b6b860792c
Paul H. Hargrove <PHHargrove@lbl.gov>
2008-03-17 06:53:11 +0000

    Marginal performance improvement for vapi/ibv conduit. When vapi-conduit was "tweaked" to try to control the layout of firehoses in Dec 2005, I made a small "thinko".  I made the same mistake twice, once for the code used by SEGMENT_FAST, and again for the code shared by SEGMENT_LARGE and SEGMENT_EVERYTHING. The "trick" for controlling layout was to truncate the length one asks for in a pin request to land the end address on a multiple of gasnetc_fh_align if possible.  The intent (now spelled out in a comment) is to encourage the eventual dense construction of regions of maximum length (which is gasnetc_fh_align*2). The code as written was doing that part just fine.  The "thinko" I made was to also limit the xfer following the firehose_local_pin() request to that same length.  In fact, the hope is that the pin request will return an existing region (hit) or if a miss it will hopefully at least produce a new region that merges with an existing adjacent region.  So, we are actually hoping that in the normal case the region returned from firehose_local_pin() will actually be longer than the truncated requested length.  This will, on average, result in the gasnet-level request being completed in fewer vapi/ibv level requests (or at least with fewer initiator-end scatter/gather segments). This commit ensures that the length of the xfer that follows a local pin will consider the full length of the resulting firehose, not the length that was truncated to encourage good alignment. Note that we cannot do this same for remote, because we don't know the actual firehose length synchronously as we do locally. This was discovered when looking to adopt the same ideas for portals-conduit.

--- ibv-conduit/gasnet_core_sndrcv.c
+++ ibv-conduit/gasnet_core_sndrcv.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_sndrcv.c,v $
- *     $Date: 2007/10/21 06:05:36 $
- * $Revision: 1.221 $
+ *     $Date: 2008/03/17 06:53:11 $
+ * $Revision: 1.222 $
  * Description: GASNet vapi conduit implementation, transport send/receive logic
  * Copyright 2003, LBNL
  * Terms of use are as specified in license.txt
@@ -347,10 +347,20 @@ gasnetc_create_cq(gasnetc_hca_hndl_t hca_hndl, gasnetc_cqe_cnt_t req_size,
   #define GASNETC_FH_LKEY(_cep, _fhptr)	((_fhptr)->client.lkey[0])
 #endif
 
+/* This limits the amount we ask for in a firehose_{local,remote}_pin() call,
+ * to enourage a steady-state layout of firehoses that has start and end addresses
+ * at multpiles of gasnetc_fh_align and length 2*gasnetc_fh_align.
+ * This will look sort of like two courses of bricks.
+ */
 GASNETI_INLINE(gasnetc_fh_aligned_len)
 size_t gasnetc_fh_aligned_len(uintptr_t start, size_t len) {
   size_t result = 2 * gasnetc_fh_align - (start & gasnetc_fh_align_mask);
   return MIN(len, result);
+}
+
+GASNETI_INLINE(gasnetc_fh_aligned_local_pin)
+const firehose_request_t *gasnetc_fh_aligned_local_pin(uintptr_t start, size_t len) {
+  return firehose_local_pin(start, gasnetc_fh_aligned_len(start, len), NULL);
 }
 
 GASNETI_INLINE(gasnetc_sr_desc_init)
@@ -2273,14 +2283,12 @@ size_t gasnetc_zerocp_common(gasnetc_epid_t epid, int rkey_index, uintptr_t loc_
       sr_desc->gasnetc_f_wr_sg_list[seg].lkey = GASNETC_SEG_LKEY(cep, base+seg);
     }
   } else {
-    const firehose_request_t *fh_loc;
+    const firehose_request_t *fh_loc = gasnetc_fh_aligned_local_pin(loc_addr, len);
     remain = len;
-    count = gasnetc_fh_aligned_len(loc_addr, len);
-    fh_loc = firehose_local_pin(loc_addr, count, NULL);
     for (seg = 0; fh_loc != NULL; ++seg) {
       sreq->fh_ptr[seg] = fh_loc;
       sreq->fh_count = seg + 1;
-      count = MIN(count, (fh_loc->addr + fh_loc->len - loc_addr));
+      count = MIN(remain, (fh_loc->addr + fh_loc->len - loc_addr));
       sr_desc->gasnetc_f_wr_sg_list[seg].addr = loc_addr;
       sr_desc->gasnetc_f_wr_sg_list[seg].gasnetc_f_sg_len = count;
       loc_addr += count;
@@ -2291,7 +2299,6 @@ size_t gasnetc_zerocp_common(gasnetc_epid_t epid, int rkey_index, uintptr_t loc_
 
       /* We hold a local firehose already, we can only 'try' or risk deadlock */
       fh_loc = firehose_try_local_pin(loc_addr, 1, NULL);
-      count = remain;
     }
     gasneti_assert(sreq->fh_count > 0);
     sr_desc->gasnetc_f_wr_num_sge = sreq->fh_count;
@@ -2736,8 +2743,9 @@ size_t gasnetc_get_local_fh(gasnetc_sreq_t *sreq, uintptr_t loc_addr, size_t len
     sreq->fh_count = i;
     len -= remain;
   } else {
-    len = gasnetc_fh_aligned_len(loc_addr, len);
-    sreq->fh_ptr[1] = firehose_local_pin(loc_addr, len, NULL);
+    const firehose_request_t *fh_loc = gasnetc_fh_aligned_local_pin(loc_addr, len);
+    len = MIN(remain, (fh_loc->addr + fh_loc->len - loc_addr));
+    sreq->fh_ptr[1] = fh_loc;
     sreq->fh_count = 2;
   }
 
