c5379971bbc9bce34ef5de9ca0be3b0b4bad59b8
Paul H. Hargrove <PHHargrove@lbl.gov>
2008-04-04 04:49:45 +0000

    Commit DISABLED icc version of ia64 lock-free lifo code (for bug 1572) Using icc 10 on Davinci, I can verify that this code compiles and that the generated assembly code *looks* correct (though reading ia64 assembly is like reading Beowulf in Old English). Since I don't have any platform to run this on, it is currently disabled.

--- gasnet_syncops.h
+++ gasnet_syncops.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/gasnet_syncops.h,v $
- *     $Date: 2008/04/04 03:50:56 $
- * $Revision: 1.46 $
+ *     $Date: 2008/04/04 04:49:45 $
+ * $Revision: 1.47 $
  * Description: GASNet header for synchronization operations used in GASNet implementation
  * Copyright 2006, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -819,6 +819,20 @@ gasneti_atomic_val_t gasneti_semaphore_trydown_partial(gasneti_semaphore_t *s, g
 	_Asm_st_volatile(_SZ_D, _LDHINT_NONE, (void *)(_addr), (int64_t)(_val))
 
       #define GASNETI_HAVE_ARCH_LIFO	1
+    #elif PLATFORM_COMPILER_INTEL
+      #include <ia64intrin.h>
+      GASNETI_INLINE(_gasneti_lifo_store16)
+      int _gasneti_lifo_store16(void volatile *ptr, uint64_t oldtag, void *newval) {
+        return oldtag != _InterlockedCompare64Exchange128_acq(ptr, (uint64_t)newval, oldtag+1, oldtag);
+      }
+      #define _gasneti_lifo_load16(_addr, _tag, _head) \
+	((_tag) = __load128((_addr), &(_head)))
+      #define _gasneti_lifo_st8_rel(_addr, _val) \
+	__st8_rel((_addr), (_val))
+
+      #if 0/* XXX: DISABLED UNTIL THIS CAN BE TESTED */
+      #define GASNETI_HAVE_ARCH_LIFO	1
+      #endif
     #elif PLATFORM_COMPILER_GNU
       GASNETI_INLINE(_gasneti_lifo_store16)
       int _gasneti_lifo_store16(void volatile *ptr, uint64_t oldtag, void *newval) {
