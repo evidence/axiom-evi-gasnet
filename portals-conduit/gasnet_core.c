60518c70fe6aca6579459cdadb4dcc8c9dcb8a08
Paul H. Hargrove <PHHargrove@lbl.gov>
2009-09-21 03:14:50 +0000

    Add on-exit() support to portals-conduit for bug 2625.

--- portals-conduit/gasnet_core.c
+++ portals-conduit/gasnet_core.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/portals-conduit/Attic/gasnet_core.c,v $
- *     $Date: 2009/09/18 23:33:40 $
- * $Revision: 1.39 $
+ *     $Date: 2009/09/21 03:14:50 $
+ * $Revision: 1.40 $
  * Description: GASNet portals conduit Implementation
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  *                 Michael Welcome <mlwelcome@lbl.gov>
@@ -20,7 +20,11 @@ GASNETI_IDENT(gasnetc_IdentString_Version, "$GASNetCoreLibraryVersion: " GASNET_
 GASNETI_IDENT(gasnetc_IdentString_Name,    "$GASNetCoreLibraryName: " GASNET_CORE_NAME_STR " $");
 
 gasnet_handlerentry_t const *gasnetc_get_handlertable(void);
+#if HAVE_ON_EXIT
+static void gasnetc_on_exit(int, void*);
+#else
 static void gasnetc_atexit(void);
+#endif
 
 gasneti_handler_fn_t gasnetc_handler[GASNETC_MAX_NUMHANDLERS]; /* handler table (recommended impl) */
 
@@ -284,7 +288,11 @@ extern int gasnetc_attach(gasnet_handlerentry_t *table, int numentries,
   gasneti_registerSignalHandlers(gasneti_defaultSignalHandler);
 #endif
 
+#if HAVE_ON_EXIT
+  on_exit(gasnetc_on_exit, NULL);
+#else
   atexit(gasnetc_atexit);
+#endif
 
   /* ------------------------------------------------------------------------------------ */
   /*  register segment  */
@@ -341,9 +349,15 @@ extern int gasnetc_attach(gasnet_handlerentry_t *table, int numentries,
   return GASNET_OK;
 }
 /* ------------------------------------------------------------------------------------ */
+#if HAVE_ON_EXIT
+static void gasnetc_on_exit(int exitcode, void *arg) {
+    gasnetc_exit(exitcode);
+}
+#else
 static void gasnetc_atexit(void) {
     gasnetc_exit(0);
 }
+#endif
 
 extern void gasnetc_exit(int exitcode) {
   /* once we start a shutdown, ignore all future SIGQUIT signals or we risk reentrancy */
