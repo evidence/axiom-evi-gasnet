89544592ebe370dffe79b95e00e6c05ba1f64339
Paul H. Hargrove <PHHargrove@lbl.gov>
2009-10-28 00:16:11 +0000

    Update extended version nummber/name for dcmf-conduit + Should never have been released as "REFERNCE" - corrected to "DCMF" + Advance version due to added native collectives (pretending prev release was 1.0)

--- dcmf-conduit/gasnet_extended_fwd.h
+++ dcmf-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/dcmf-conduit/gasnet_extended_fwd.h,v $
- *     $Date: 2009/10/17 00:01:07 $
- * $Revision: 1.5 $
+ *     $Date: 2009/10/28 00:16:11 $
+ * $Revision: 1.6 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2008, Rajesh Nishtala <rajeshn@cs.berkeley.edu>
  *                 Dan Bonachea <bonachea@cs.berkeley.edu>
@@ -14,9 +14,9 @@
 #ifndef _GASNET_EXTENDED_FWD_H
 #define _GASNET_EXTENDED_FWD_H
 
-#define GASNET_EXTENDED_VERSION      GASNET_RELEASE_VERSION_MAJOR.GASNET_RELEASE_VERSION_MINOR
+#define GASNET_EXTENDED_VERSION      1.1
 #define GASNET_EXTENDED_VERSION_STR  _STRINGIFY(GASNET_EXTENDED_VERSION)
-#define GASNET_EXTENDED_NAME         REFERENCE
+#define GASNET_EXTENDED_NAME         DCMF
 #define GASNET_EXTENDED_NAME_STR     _STRINGIFY(GASNET_EXTENDED_NAME)
 
 
