2af961c2ebefda42c477d780a39b3f53b3437971
Paul H. Hargrove <PHHargrove@lbl.gov>
2009-10-28 00:27:10 +0000

    Distinguish smp-conduit extended implementation from the reference impl. + we now have conduit-specific barrier and collective code for smp-conduit.

--- smp-conduit/gasnet_extended_fwd.h
+++ smp-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/smp-conduit/gasnet_extended_fwd.h,v $
- *     $Date: 2009/09/18 23:33:46 $
- * $Revision: 1.2 $
+ *     $Date: 2009/10/28 00:27:10 $
+ * $Revision: 1.3 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -13,9 +13,9 @@
 #ifndef _GASNET_EXTENDED_FWD_H
 #define _GASNET_EXTENDED_FWD_H
 
-#define GASNET_EXTENDED_VERSION      GASNET_RELEASE_VERSION_MAJOR.GASNET_RELEASE_VERSION_MINOR
+#define GASNET_EXTENDED_VERSION      1.0
 #define GASNET_EXTENDED_VERSION_STR  _STRINGIFY(GASNET_EXTENDED_VERSION)
-#define GASNET_EXTENDED_NAME         REFERENCE
+#define GASNET_EXTENDED_NAME         SMP
 #define GASNET_EXTENDED_NAME_STR     _STRINGIFY(GASNET_EXTENDED_NAME)
 
 
