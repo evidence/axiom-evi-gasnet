873f7cf87a485d86b8b07f8262017c3a92fbc1ee
Paul H. Hargrove <PHHargrove@lbl.gov>
2010-03-08 08:12:40 +0000

    Follow up on previous commit by eliminating smp-conduit/gasnet_extended.c

--- smp-conduit/gasnet_extended_fwd.h
+++ smp-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/smp-conduit/gasnet_extended_fwd.h,v $
- *     $Date: 2009/10/28 00:27:10 $
- * $Revision: 1.3 $
+ *     $Date: 2010/03/08 08:12:40 $
+ * $Revision: 1.4 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -27,6 +27,16 @@ typedef struct _gasnete_op_t *gasnet_handle_t;
 #define GASNETI_EOP_IS_HANDLE 1
 
 #define GASNETE_COLL_CONDUIT_BARRIERS GASNETE_COLL_BARRIER_PSHM
+#define GASNETE_BARRIER_DEFAULT "PSHM"
+#define GASNETE_BARRIER_READENV() do { \
+    if(GASNETE_ISBARRIER("PSHM")) gasnete_coll_default_barrier_type = GASNETE_COLL_BARRIER_PSHM; \
+  } while (0)
+#define GASNETE_BARRIER_INIT(TEAM, BARRIER_TYPE) do {       \
+    if ((BARRIER_TYPE) == GASNETE_COLL_BARRIER_PSHM &&      \
+        (TEAM) == GASNET_TEAM_ALL) {                        \
+      gasnete_pshmbarrier_init(TEAM);                       \
+    }                                                       \
+  } while (0)
 
   /* this can be used to add statistical collection values 
      specific to the extended API implementation (see gasnet_help.h) */
