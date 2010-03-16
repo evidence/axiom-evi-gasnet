44bcbb52defdbf8622330296a60b72eec8bf3d99
Paul H. Hargrove <PHHargrove@lbl.gov>
2010-03-16 21:02:59 +0000

    Add a -p option to testbarrier. As in testam this causes a PAR build to spawn extra threads that just sit in a BLOCKUNTIL.  This is intended for looking at the effect of additional threads running the barrier's progress function and/or AM handler(s).

--- tests/testbarrier.c
+++ tests/testbarrier.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/tests/testbarrier.c,v $
- *     $Date: 2006/01/28 21:21:46 $
- * $Revision: 1.19 $
+ *     $Date: 2010/03/16 21:02:59 $
+ * $Revision: 1.20 $
  * Description: GASNet barrier performance test
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -10,27 +10,80 @@
 
 #include <test.h>
 
+int mynode, nodes, iters = 0;
+
+#define hidx_done_shorthandler   200
+volatile int done = 0;
+void done_shorthandler(gasnet_token_t token) {
+	  done = 1;
+}
+gasnet_handlerentry_t htable[] = { { hidx_done_shorthandler,  done_shorthandler  } };
+
+static void * doTest(void *);
+
 int main(int argc, char **argv) {
-  int mynode, nodes, iters=0;
-  int64_t start,total;
-  int i = 0;
+  int pollers = 0;
+  int arg = 1;
 
   GASNET_Safe(gasnet_init(&argc, &argv));
-  GASNET_Safe(gasnet_attach(NULL, 0, TEST_SEGSZ_REQUEST, TEST_MINHEAPOFFSET));
+  GASNET_Safe(gasnet_attach(htable, 1, TEST_SEGSZ_REQUEST, TEST_MINHEAPOFFSET));
+
+#if GASNET_PAR
+  test_init("testbarrier", 1, "[-p polling_threads] (iters)");
+#else
   test_init("testbarrier", 1, "(iters)");
+#endif
   mynode = gasnet_mynode();
   nodes = gasnet_nodes();
 
-  if (argc > 1) iters = atoi(argv[1]);
-  if (!iters) iters = 10000;
-  if (argc > 2) test_usage();
-
-  if (mynode == 0) {
-      printf("Running barrier test with %i iterations...\n",iters);
-      fflush(stdout);
+  if ((argc-arg >= 2) && !strcmp(argv[arg], "-p")) {
+#if GASNET_PAR
+    pollers = atoi(argv[arg+1]);
+    arg += 2;
+#else
+    if (gasnet_mynode() == 0) {
+      fprintf(stderr, "testbarrier %s\n", GASNET_CONFIG_STRING);
+      fprintf(stderr, "ERROR: The -p option is only available in the PAR configuration.\n");
+      fflush(NULL);
+    }
+    sleep(1);
+    gasnet_exit(1);
+#endif
   }
+  if (argc-arg >= 1) iters = atoi(argv[arg]);
+  if (!iters) iters = 10000;
+  if (argc-arg >= 2) test_usage();
+
+#if !defined(GASNET_PAR)
+  MSG0("Running barrier test with %i iterations...\n",iters);
+#else
+  MSG0("Running barrier test with %i iterations and %i extra polling threads...\n",iters,pollers);
+
+  if (pollers)
+      test_createandjoin_pthreads(pollers+1,doTest,NULL,0);
+  else
+#endif
+      doTest(NULL);
+
   BARRIER();
 
+  MSG("done.");
+
+  gasnet_exit(0);
+  return 0;
+}
+
+static void * doTest(void *arg) {
+  int64_t start,total;
+  int i = 0;
+
+  if (arg) {
+    /* I am a polling thread */
+    GASNET_BLOCKUNTIL(done);
+    return NULL;
+  }
+
+  BARRIER();
   start = TIME();
   for (i=0; i < iters; i++) {
     gasnet_barrier_notify(i, 0);            
@@ -62,10 +115,6 @@ int main(int argc, char **argv) {
       fflush(stdout);
   }
 
-  BARRIER();
-
-  MSG("done.");
-
-  gasnet_exit(0);
-  return 0;
+  GASNET_Safe(gasnet_AMRequestShort0(mynode, hidx_done_shorthandler));
+  return NULL;
 }
