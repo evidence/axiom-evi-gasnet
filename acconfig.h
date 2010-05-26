5e20230959f224f8f68c72db9c2d4fdf0d97b741
Paul H. Hargrove <PHHargrove@lbl.gov>
2010-05-26 03:54:33 +0000

    Bug 2809 (SRQ support in IBV conduit) Step 5 This commit implements a --disable-ibv-srq configure argument This also makes cosmetic changes to other "configure --help" output A follow up commit is expected to simplify some of the #if's

--- acconfig.h
+++ acconfig.h
@@ -1,6 +1,6 @@
 /*    $Source: /Users/kamil/work/gasnet-cvs2/gasnet/acconfig.h,v $ */
-/*      $Date: 2010/01/27 22:41:12 $ */
-/*  $Revision: 1.151 $ */
+/*      $Date: 2010/05/26 03:54:33 $ */
+/*  $Revision: 1.152 $ */
 /*  Description: GASNet acconfig.h (or config.h)                             */
 /*  Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>                  */
 /* Terms of use are as specified in license.txt */
@@ -320,6 +320,7 @@
 #undef GASNETC_VAPI_MAX_HCAS
 
 /* GASNet ibv-conduit features and bug work-arounds */
+#undef HAVE_IBV_SRQ
 #undef GASNETC_IBV_POLL_LOCK
 #undef GASNETC_IBV_RCV_THREAD
 #undef GASNETC_IBV_MAX_HCAS
