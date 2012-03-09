a6749f2e8bc0216f90954357c027897454a7d98e
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-03-09 03:08:46 +0000

    Implement GASNet analogue of the pthread stack size controls in UPCR. These will allow automatic increases (by logic yet-to-be-written in upcrun.pl) in the case of especially large TLS requirements.

--- ibv-conduit/gasnet_core_thread.c
+++ ibv-conduit/gasnet_core_thread.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_thread.c,v $
- *     $Date: 2012/03/09 00:25:37 $
- * $Revision: 1.13 $
+ *     $Date: 2012/03/09 03:08:46 $
+ * $Revision: 1.14 $
  * Description: GASNet vapi/ibv conduit implementation, progress thread logic
  * Copyright 2012, LBNL
  * Terms of use are as specified in license.txt
@@ -161,9 +161,49 @@ static void * gasnetc_progress_thread(void *arg)
 extern void
 gasnetc_spawn_progress_thread(gasnetc_progress_thread_t *pthr_p)
 {
+  static gasneti_mutex_t init_lock = GASNETI_MUTEX_INITIALIZER;
+  static size_t stack_sz = 0;
   pthread_attr_t attr;
   pthread_attr_init(&attr);
+
+  gasneti_mutex_lock(&init_lock);
+  if (!stack_sz) {
+    /* Code to determine proper stack size */
+    const size_t stack_min = gasneti_getenv_int_withdefault("GASNET_THREAD_STACK_MIN", 0, 1);
+    const size_t stack_pad = gasneti_getenv_int_withdefault("GASNET_THREAD_STACK_PAD", 0, 1);
+    size_t stack_dflt;
+  #ifdef RLIMIT_STACK
+    gasnett_maximize_rlimit(RLIMIT_STACK);
+  #endif
+    gasneti_assert_zeroret(pthread_attr_getstacksize(&attr, &stack_dflt));
+    stack_sz = MAX(stack_min, stack_dflt + stack_pad);
+    stack_sz = GASNETI_PAGE_ALIGNUP(stack_sz);
+    if (0 != pthread_attr_setstacksize(&attr, stack_sz)) {
+      /* Binary search between default (lo) and requested (hi) */
+      size_t lo = stack_dflt;
+      size_t hi = stack_sz;
+    
+      while (lo != (stack_sz = lo + GASNETI_PAGE_ALIGNUP((hi - lo) >> 1))) {
+        int rc = pthread_attr_setstacksize(&attr, stack_sz);
+        if (rc == 0) {
+          lo = stack_sz;
+        } else if (rc == EINVAL){
+          hi = stack_sz;
+        } else {
+          /* Unknown error: silently give up and use the default */
+          stack_sz = stack_dflt;
+          break;
+        }
+      }
+    }
+    GASNETI_TRACE_PRINTF(I, ("Stack size for progress thread(s) set to %lu%s",
+                              (unsigned long)stack_sz,
+                              (stack_sz == stack_dflt) ? " (default)" : ""));
+  }
+  gasneti_mutex_unlock(&init_lock);
+
   (void)pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM); /* ignore failures */
+  gasneti_assert_zeroret(pthread_attr_setstacksize(&attr, stack_sz));
   gasneti_assert_zeroret(pthread_create(&pthr_p->thread_id, &attr, gasnetc_progress_thread, pthr_p));
   gasneti_assert_zeroret(pthread_attr_destroy(&attr));
   GASNETI_TRACE_PRINTF(I, ("Spawned progress thread with id 0x%lx",
