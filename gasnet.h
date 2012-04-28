661f418182eef958efd2ad8cdd9b83562d2fad07
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-04-28 00:08:22 +0000

    Switch config string "(no)debug_malloc" to "debugmalloc" (no underscore). This is the last chance before this feature appears in a release. Before this was added, all gasnet configs strings which contained an underscore used it as '='.  This change restores this invariant.

--- gasnet.h
+++ gasnet.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/gasnet.h,v $
- *     $Date: 2012/01/06 23:43:46 $
- * $Revision: 1.72 $
+ *     $Date: 2012/04/28 00:08:22 $
+ * $Revision: 1.73 $
  * Description: GASNet Header
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -90,9 +90,9 @@
 #ifdef GASNET_DEBUGMALLOC
   #undef GASNET_DEBUGMALLOC
   #define GASNET_DEBUGMALLOC 1
-  #define GASNETI_MALLOC_CONFIG debug_malloc
+  #define GASNETI_MALLOC_CONFIG debugmalloc
 #else
-  #define GASNETI_MALLOC_CONFIG nodebug_malloc
+  #define GASNETI_MALLOC_CONFIG nodebugmalloc
 #endif
 
 #if defined(GASNET_SRCLINES) || defined(GASNET_DEBUG)
