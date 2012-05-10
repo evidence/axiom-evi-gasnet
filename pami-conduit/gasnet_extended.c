c1f9b6e5e0ac797300837aab284836c9dfcdda8c
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-05-10 00:40:18 +0000

    Implement gasnete_try_syncnb_nopoll() for pami-conduit

--- pami-conduit/gasnet_extended.c
+++ pami-conduit/gasnet_extended.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/pami-conduit/gasnet_extended.c,v $
- *     $Date: 2012/04/24 17:39:52 $
- * $Revision: 1.18 $
+ *     $Date: 2012/05/10 00:40:18 $
+ * $Revision: 1.19 $
  * Description: GASNet Extended API PAMI-conduit Implementation
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Copyright 2012, Lawrence Berkeley National Laboratory
@@ -608,6 +608,15 @@ extern gasnet_handle_t gasnete_memset_nb   (gasnet_node_t node, void *dest, int 
   Synchronization for explicit-handle non-blocking operations:
   ===========================================================
 */
+
+extern int  gasnete_try_syncnb_nopoll(gasnet_handle_t handle) {
+  if (gasnete_op_isdone(handle)) {
+    gasneti_sync_reads();
+    gasnete_op_free(handle);
+    return GASNET_OK;
+  }
+  else return GASNET_ERR_NOT_READY;
+}
 
 extern int  gasnete_try_syncnb(gasnet_handle_t handle) {
   GASNETI_SAFE(gasneti_AMPoll());
