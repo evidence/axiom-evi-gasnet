dab5743a4bc84c9061de3d56b52f79df629d6e6a
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-05-10 00:54:38 +0000

    Implement gasnete_try_syncnb_nopoll() for shmem-conduit.

--- shmem-conduit/gasnet_extended_help_extra.h
+++ shmem-conduit/gasnet_extended_help_extra.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/shmem-conduit/gasnet_extended_help_extra.h,v $
- *     $Date: 2010/12/16 23:44:08 $
- * $Revision: 1.11 $
+ *     $Date: 2012/05/10 00:54:38 $
+ * $Revision: 1.12 $
  * Description: GASNet Extended Shmem-specific Header 
  * Copyright 2005, Christian Bell <csbell@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -446,6 +446,7 @@ _gasnete_try_syncnb(gasnet_handle_t handle)
     return GASNETE_OK;
 }
 #define gasnete_try_syncnb(handle)	_gasnete_try_syncnb(handle)
+#define gasnete_try_syncnb_nopoll(handle)	_gasnete_try_syncnb(handle)
 
 GASNETI_INLINE(_gasnete_try_syncnb_some)
 int _gasnete_try_syncnb_some(gasnet_handle_t *phandle, size_t numhandles) {
