4bd414159b945819eb06d9209f442f14aaac7810
Paul H. Hargrove <PHHargrove@lbl.gov>
2013-02-11 23:29:18 +0000

    Implement cross-machine mode (-c option) in testam. Most of this patch is actually devoted to generalizing the arg parsing.

--- tests/testam.c
+++ tests/testam.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/tests/testam.c,v $
- *     $Date: 2012/04/14 00:37:55 $
- * $Revision: 1.34 $
+ *     $Date: 2013/02/11 23:29:18 $
+ * $Revision: 1.35 $
  * Description: GASNet Active Messages performance test
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -14,6 +14,7 @@ uintptr_t maxsz = 0;
 #include <test.h>
 
 int mynode = 0;
+int numnode = 0;
 void *myseg = NULL;
 int sender, recvr;
 int peer;
@@ -22,7 +23,7 @@ void *peerseg = NULL;
 void report(const char *desc, int64_t totaltime, int iters, uintptr_t sz, int rt) {
   if (sender) {
       char nodestr[10];
-      if (gasnet_nodes() > 2) snprintf(nodestr,sizeof(nodestr),"%i: ",mynode);
+      if (numnode > 2) snprintf(nodestr,sizeof(nodestr),"%i: ",mynode);
       else nodestr[0] = '\0';
       printf("%c: %s%-46s: %6.3f sec %8.3f us", TEST_SECTION_NAME(),
         nodestr, desc, ((double)totaltime)/1000000, ((double)totaltime)/iters);
@@ -118,6 +119,7 @@ void done_shorthandler(gasnet_token_t token) {
 }
 
 /* ------------------------------------------------------------------------------------ */
+int crossmachinemode = 0;
 int iters=0;
 int pollers=0;
 int i = 0;
@@ -125,6 +127,7 @@ uintptr_t maxmed, maxlongreq, maxlongrep;
 void *doAll(void*);
 
 int main(int argc, char **argv) {
+  int help=0;
   int arg=1;
 
   gasnet_handlerentry_t htable[] = { 
@@ -147,35 +150,48 @@ int main(int argc, char **argv) {
 
   GASNET_Safe(gasnet_init(&argc, &argv));
 
-  if ((argc-arg >= 2) && !strcmp(argv[arg], "-p")) {
+  mynode = gasnet_mynode();
+  numnode = gasnet_nodes();
+
+  arg = 1;
+  while (argc > arg) {
+    if (!strcmp(argv[arg], "-p")) {
 #if GASNET_PAR
-    pollers = atoi(argv[arg+1]);
-    arg += 2;
+      pollers = atoi(argv[arg+1]);
+      arg += 2;
 #else
-    if (gasnet_mynode() == 0) {
-      fprintf(stderr, "testam %s\n", GASNET_CONFIG_STRING);
-      fprintf(stderr, "ERROR: The -p option is only available in the PAR configuration.\n");
-      fflush(NULL);
-    }
-    sleep(1);
-    gasnet_exit(1);
+      if (0 == mynode) {
+        fprintf(stderr, "testam %s\n", GASNET_CONFIG_STRING);
+        fprintf(stderr, "ERROR: The -p option is only available in the PAR configuration.\n");
+        fflush(NULL);
+      }
+      sleep(1);
+      gasnet_exit(1);
 #endif
+    } else if (!strcmp(argv[arg], "-c")) {
+      crossmachinemode = 1;
+      ++arg;
+    } else if (argv[arg][0] == '-') {
+      help = 1;
+      ++arg;
+    } else break;
   }
-  if (argc-arg >= 1) iters = atoi(argv[arg]);
+
+  if (argc > arg) { iters = atoi(argv[arg]); ++arg; }
   if (!iters) iters = 1000;
-  if (argc-arg >= 2) maxsz = atoi(argv[arg+1]);
+  if (argc > arg) { maxsz = atoi(argv[arg]); ++arg; }
   if (!maxsz) maxsz = 2*1024*1024;
-  if (argc-arg >= 3) TEST_SECTION_PARSE(argv[arg+2]);
+  if (argc > arg) { TEST_SECTION_PARSE(argv[arg]); ++arg; }
 
   GASNET_Safe(gasnet_attach(htable, sizeof(htable)/sizeof(gasnet_handlerentry_t),
                             TEST_SEGSZ_REQUEST, TEST_MINHEAPOFFSET));
+  test_init("testam", 1, "[options] (iters) (maxsz) (test_sections)\n"
 #if GASNET_PAR
-  test_init("testam", 1, "[-p polling_threads] (iters) (maxsz) (test_sections)");
-#else
-  test_init("testam", 1, "(iters) (maxsz) (test_sections)");
+               "  The -p option gives the number of polling threads, specified as\n"
+               "    a non-negative integer argument (default is no polling threads).\n"
 #endif
-  if (argc-arg >= 4) test_usage();
-  mynode = gasnet_mynode();
+               "  The -c option enables cross-machine pairing (default is nearest neighbor).\n");
+  if (help || argc > arg) test_usage();
 
   TEST_PRINT_CONDUITINFO();
 
@@ -184,23 +200,42 @@ int main(int argc, char **argv) {
   maxmed = MIN(maxsz, gasnet_AMMaxMedium());
   maxlongreq = MIN(maxsz, gasnet_AMMaxLongRequest());
   maxlongrep = MIN(maxsz, gasnet_AMMaxLongReply());
-  peer = mynode ^ 1;
-  if (peer == gasnet_nodes()) {
-    /* w/ odd # of nodes, last one talks to self */
-    peer = mynode;
+
+  if (crossmachinemode) {
+    if ((numnode%2) && (mynode == numnode-1)) {
+      sender = 1;
+      peer = mynode;
+    } else {
+      gasnet_node_t half = numnode / 2;
+      sender = (mynode < half);
+      peer = sender ? (mynode + half) : (mynode - half);
+    }
+  } else {
+    peer = mynode ^ 1;
+    sender = mynode % 2 == 0;
+    if (peer == numnode) {
+      peer = mynode;
+    }
   }
+
+  recvr = !sender || (peer == mynode);
+
   peerseg = TEST_SEG(peer);
-  sender = mynode % 2 == 0;
-  recvr = (!sender || (gasnet_nodes()%2 == 1 && mynode == gasnet_nodes()-1));
 
   BARRIER();
 
   if (mynode == 0) {
+      printf("Running %sAM performance test with %i iterations"
 #if GASNET_PAR
-      printf("Running AM performance test with %i iterations and %i extra recvr polling threads...\n",iters,pollers);
-#else
-      printf("Running AM performance test with %i iterations...\n",iters);
+             " and %i extra recvr polling threads"
 #endif
+             "...\n",
+             (crossmachinemode ? "cross-machine ": ""),
+             iters
+#if GASNET_PAR
+             ,pollers
+#endif
+            );
       printf("   Msg Sz  Description                             Total time   Avg. time   Bandwidth\n"
              "   ------  -----------                             ----------   ---------   ---------\n");
       fflush(stdout);
@@ -317,7 +352,7 @@ void doAMShort(void) {
         if (!sz) sz = 1;                            \
         else if (sz < maxsz && sz*2 > maxsz) {      \
            /* indicate final non-power-of-two sz */ \
-           if (!gasnet_mynode()) printf(" max:\n"); \
+           if (!mynode) printf(" max:\n"); \
            sz = maxsz;                              \
         } else sz *= 2;                             \
   } while (0)
