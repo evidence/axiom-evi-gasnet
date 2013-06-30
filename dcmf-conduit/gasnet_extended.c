6cb35b1e6ffa6e5ff8c3defdc7995bb32edff085
Paul H. Hargrove <PHHargrove@lbl.gov>
2013-06-30 23:11:54 +0000

    update dcmf-conduit to latest reference

--- dcmf-conduit/gasnet_extended.c
+++ dcmf-conduit/gasnet_extended.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/dcmf-conduit/gasnet_extended.c,v $
- *     $Date: 2013/06/30 21:25:59 $
- * $Revision: 1.56 $
+ *     $Date: 2013/06/30 23:11:54 $
+ * $Revision: 1.57 $
  * Description: GASNet Extended API Implementation for DCMF
  * Copyright 2008, Rajesh Nishtala <rajeshn@cs.berkeley.edu>
  *                 Dan Bonachea <bonachea@cs.berkeley.edu>
@@ -263,13 +263,8 @@ static
 void gasnete_op_markdone(gasnete_op_t *op, int isget) {
   if (OPTYPE(op) == OPTYPE_EXPLICIT) {
     gasnete_eop_t *eop = (gasnete_eop_t *)op;
-    gasneti_assert(!GASNETE_EOP_DONE(eop));
     gasnete_eop_check(eop);
-#if GASNETE_EOP_COUNTED
-    gasneti_weakatomic_increment(&(eop->completed_cnt), 0);
-#else
-    SET_OPSTATE(eop, OPSTATE_COMPLETE);
-#endif
+    GASNETE_EOP_MARKDONE(eop);
   } else {
     gasnete_iop_t *iop = (gasnete_iop_t *)op;
     gasnete_iop_check(iop);
@@ -375,7 +370,7 @@ extern void gasnete_init(void) {
 
     /* cause the first pool of eops to be allocated (optimization) */
     eop = gasnete_eop_new(threaddata);
-    gasnete_op_markdone((gasnete_op_t *)eop, 0);
+    GASNETE_EOP_MARKDONE(eop);
     gasnete_eop_free(eop);
   }
 #if GASNETE_DIRECT_PUT_GET
@@ -465,7 +460,9 @@ gasneti_iop_t *gasneti_iop_register(unsigned int noperations, int isget GASNETE_
   return (gasneti_iop_t *)op;
 }
 void gasneti_eop_markdone(gasneti_eop_t *eop) {
-  gasnete_op_markdone((gasnete_op_t *)eop, 0);
+  gasnete_eop_t *op = (gasnete_eop_t *)eop;
+  gasnete_eop_check(op);
+  GASNETE_EOP_MARKDONE(op);
 }
 void gasneti_iop_markdone(gasneti_iop_t *iop, unsigned int noperations, int isget) {
   gasnete_iop_t *op = (gasnete_iop_t *)iop;
