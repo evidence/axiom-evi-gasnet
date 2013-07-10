b7b3acd3ed31c05a3efcf9b9bbf2de478e7dd49a
Paul H. Hargrove <PHHargrove@lbl.gov>
2013-07-10 21:49:19 +0000

    Corrections to spawner environment logic in mxm-conduit + env var was changed from GASNET_SPAWNER to GASNET_MXM_SPAWNER + ssh/mpi should be all caps (error cloned from vapi/ibv conduit)

--- mxm-conduit/gasnet_core.c
+++ mxm-conduit/gasnet_core.c
@@ -157,9 +157,8 @@ static void gasneti_bootstrapInit(
     gasnet_node_t *nodes_p,
     gasnet_node_t *mynode_p)
 {
-    char *spawner = gasneti_getenv_withdefault("GASNET_SPAWNER", "(not set)");
-    if (!strcmp(spawner, "ssh") ||
-            ((*argc_p >= 2) && !strncmp((*argv_p)[1], "-GASNET-SPAWN-", 14))) {
+    char *spawner = gasneti_getenv_withdefault("GASNET_MXM_SPAWNER", "(not set)");
+    if (!strcmp(spawner, "SSH")) {
 #if HAVE_SSH_SPAWNER
         gasneti_bootstrapInit_ssh(argc_p, argv_p, nodes_p, mynode_p);
         gasneti_bootstrapFini_p     = &gasneti_bootstrapFini_ssh;
@@ -181,7 +180,7 @@ static void gasneti_bootstrapInit(
         gasneti_bootstrapAlltoall_p	= &gasneti_bootstrapAlltoall_mpi;
         gasneti_bootstrapBroadcast_p= &gasneti_bootstrapBroadcast_mpi;
 #else
-        if (!strcmp(spawner, "mpi")) {
+        if (!strcmp(spawner, "MPI")) {
             gasneti_fatalerror("Requested mpi-spawner is not supported in this build");
         } else {
             gasneti_fatalerror("Requested spawner \"%s\" is unknown", spawner);
