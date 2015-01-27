evidence/axiom-evi-gasnet

commit 924a4c2312771d0e72618799bbea186d00f4944c

Implement GASNET_MAXIMIZE__RLIMIT_* env vars
This commit implements a family of environment variables which can
be used to suppress the maximization of individual rlimits within
gasnett_maximize_rlimit() and gasnett_maximize_rlimits().

This provides the functionality requested in pull-request #19.

The documentation in README-tools is also updated to describe this
new behavior.

gasnet_tools.c
@@ -1802,6 +1802,12 @@ int gasnett_maximize_rlimits(void) {
 }
 int gasnett_maximize_rlimit(int res, const char *lim_desc) {
   int success = 0;
+
+  char ctrl_var[32] = "GASNET_MAXIMIZE_";
+  gasneti_assert(strlen(ctrl_var) + strlen(lim_desc) < sizeof(ctrl_var));
+  if (!gasneti_getenv_yesno_withdefault(strncat(ctrl_var, lim_desc, sizeof(ctrl_var)), 1))
+    return 1;
+
   #ifdef __USE_GNU
     /* workaround an annoying glibc header bug, which erroneously declares get/setrlimit to take 
        the enum type __rlimit_resource_t, instead of int as required by POSIX */

README-tools
@@ -1057,9 +1057,15 @@ int gasnett_maximize_rlimits();
 int gasnett_maximize_rlimit(int res, const char *lim_desc);
  
    Maximize an rlimit indicated by res (an RLIMIT_* constant from sys/resource.h), with
-   associated limit description. gasnett_maximize_rlimits() maximizes all the in-memory 
-   execution rlimits associated with the current process (does not affect file system 
-   related limits).
+   associated limit description. gasnett_maximize_rlimits() maximizes cpu time and all
+   the in-memory execution rlimits associated with the current process (does not affect
+   file system related limits). These functions return non-zero on success.
+
+   If the environment variable GASNET_MAXIMIZE_[desc] is set to a false value (as
+   defined by gasnett_getenv_yesno_withdefault()) then the corresponding limit will
+   NOT be maximized, though the result will still indicate success.  For instance,
+   setting GASNET_MAXIMIZE_RLIMIT_CPU=0 will suppress maximizing the limit on cpu
+   time, but will not (by itself) cause a zero (failure) return value.
   
 ---------------
 Feature control

commit 1194a2fd5665d6d6e8f4803cb776e03af5b45882

Fix a regression introduced in 924a4c2
This commit relocates the new logic in gasneti_maximize_rlimit()
to make it conform to C89 (no mid-block decls).

gasnet_tools.c
@@ -1803,11 +1803,6 @@ int gasnett_maximize_rlimits(void) {
 int gasnett_maximize_rlimit(int res, const char *lim_desc) {
   int success = 0;
 
-  char ctrl_var[32] = "GASNET_MAXIMIZE_";
-  gasneti_assert(strlen(ctrl_var) + strlen(lim_desc) < sizeof(ctrl_var));
-  if (!gasneti_getenv_yesno_withdefault(strncat(ctrl_var, lim_desc, sizeof(ctrl_var)), 1))
-    return 1;
-
   #ifdef __USE_GNU
     /* workaround an annoying glibc header bug, which erroneously declares get/setrlimit to take 
        the enum type __rlimit_resource_t, instead of int as required by POSIX */
@@ -1822,6 +1817,11 @@ int gasnett_maximize_rlimit(int res, const char *lim_desc) {
   #else
     #define RLIM_CALL(fnname,structname) fnname
   #endif
+
+  char ctrl_var[32] = "GASNET_MAXIMIZE_";
+  gasneti_assert(strlen(ctrl_var) + strlen(lim_desc) < sizeof(ctrl_var));
+  if (!gasneti_getenv_yesno_withdefault(strncat(ctrl_var, lim_desc, sizeof(ctrl_var)), 1))
+    return 1;
 
   #define SET_RLIMITS(structname, getrlimit, setrlimit) do {                                    \
     structname oldval,newval;                                                                   \