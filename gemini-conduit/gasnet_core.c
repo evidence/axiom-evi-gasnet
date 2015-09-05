98d579d18f10a1e5bbcb4503f971b804640fb45c
Paul H. Hargrove <PHHargrove@lbl.gov>
2015-09-04 21:54:08 -0700

    aries/gemini: disable mpi interoperability "trick"
    This commit disables (via "#if 0") a "trick" introduced to help get
    better default MPI interoperability.  It was thought that the approach
    had no risk of creating problems, but we now have a report of at least
    one GASNet + MPI application that does not work unless this is
    disabled.

--- gemini-conduit/gasnet_core.c
+++ gemini-conduit/gasnet_core.c
@@ -125,16 +125,11 @@ static int gasnetc_bootstrapInit(int *argc, char ***argv) {
    * 3) If you don't want a build of GASNet that assumes the order the two
    *    libaries will be initialized, then clone the third variant into the
    *    application, to run between the two initializations.
-   *
-   * NOTE: by default we have enabled "Option 2" which advances the cookie
-   * value in the environment after GASNet has read it.  In the event that
-   * MPI is initialized second, this probably "just works".  In the event
-   * that MPI is not used (a non-hybrid application) then this is harmless.
    */
   #if 0   /* Option 1) If MPI is initialized first enable this: */
     gasnetc_cookie += 1;
   #endif
-  #if 1   /* Option 2) If GASNet is initialized first enable this: */
+  #if 0   /* Option 2) If GASNet is initialized first enable this: */
   { char cookie[10];
     sprintf(cookie, "%u", gasnetc_cookie + 1);
     gasnett_setenv("PMI_GNI_COOKIE", cookie);
