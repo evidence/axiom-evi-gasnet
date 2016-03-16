evidence/axiom-evi-gasnet

commit d52c78aca9a00977a40e99e0d9c843db13eb59dc

firehose: minor changes for checkpoint support
At least for ibv-conduit we need to deregister all memory before one
can take a checkpoint with BLCR.  This includes, of course, the
registrations which the firehose library is managing.  In the future
I hope to have a graceful way to deal with this, but for now we are
simply calling firehose_fini() before the checkpoint, and then will
call firehose_init() after the checkpoint.

This commit makes some minor changes to firehose_fini() to make it
possible to call firehose_init() after a call to firehose_fini().  As
curently implemented one needs to pass a new flag to firehose_init().
Without that flag we retain the preexisting behavior of returning from
firehose_fini() with the 2 main locks still held (which is intended to
help catch races or coding errors which may lead to calls after fini).

other/firehose/firehose.c
@@ -163,8 +163,7 @@ firehose_init(uintptr_t max_pinnable_memory,
 #define FH_REQUEST_ALLOC_PERIDX	256
 #define FH_REQUEST_ALLOC_MAXIDX	256
 static firehose_request_t	*fh_request_bufs[FH_REQUEST_ALLOC_MAXIDX] = { 0 };
-
-extern int fh_dacount;
+static int			 fh_request_bufidx = 0;
 
 /*
  * XXX should call from gasnet_exit(), fatal or not
@@ -175,7 +174,8 @@ firehose_fini()
 {
 	int	i;
 
-	/* Acquire the lock(s) and DO NOT release */
+	/* Acquire the lock(s)
+	 * DO NOT release unless FIREHOSE_INIT_FLAG_MAY_REINIT */
 	FH_TABLE_LOCK;
 #ifndef FH_POLL_NOOP
 	FH_POLLQ_LOCK;
@@ -204,7 +204,17 @@ firehose_fini()
 		if (fh_request_bufs[i] == NULL)
 			break;
 		gasneti_free(fh_request_bufs[i]);
+		fh_request_bufs[i] == NULL;
 	}
+        fh_request_freehead = NULL;
+        fh_request_bufidx = 0;
+
+        if (fhi_InitFlags & FIREHOSE_INIT_FLAG_MAY_REINIT) {
+#ifndef FH_POLL_NOOP
+                FH_POLLQ_UNLOCK;
+#endif
+                FH_TABLE_UNLOCK;
+        }
 
 	return;
 }
@@ -572,7 +582,6 @@ fh_free_completion_callback(fh_completion_callback_t *cc)
  * latter case, allocation is done using a freelist allocator and the internal
  * pointer is used to link the request_t.
  */
-static int			 fh_request_bufidx = 0;
 
 static firehose_request_t *
 fh_request_new(firehose_request_t *ureq, int block)

other/firehose/firehose.h
@@ -416,9 +416,16 @@ firehose_init(uintptr_t max_pinnable_memory,
  *   only if you believe that the system does not cleanup properly when
  *   a process exits.
  *   CURRENTLY ONLY IMPLEMENTED FOR FIREHOSE-REGION
+ *
+ * FIREHOSE_INIT_FLAG_MAY_REINIT
+ *   If this flag is set, firehose_fini() will leave the state suitable
+ *   for a possible subsequent call to firehose_init() with the same
+ *   flags as the initial call.  Without this flag any call make to
+ *   firehose_init() after firehose_fini() has undefined behavior.
  */
 #define FIREHOSE_INIT_FLAG_LOCAL_ONLY		0x01
 #define FIREHOSE_INIT_FLAG_UNPIN_ON_FINI	0x02
+#define FIREHOSE_INIT_FLAG_MAY_REINIT		0x04
 
 /************************
  * Firehose Finalization

commit f68854073930e3e3411106f66c6921ba0f8bcddb

firehose: fix typo in checkpoint support
This commit corrects use of "==" where "=" was intended/required.
This error was introduced in d52c78aca9a00977a40e99e0d9c843db13eb59dc

This commit cherry-picked from branch 'feature/blcr'

other/firehose/firehose.c
@@ -204,7 +204,7 @@ firehose_fini()
 		if (fh_request_bufs[i] == NULL)
 			break;
 		gasneti_free(fh_request_bufs[i]);
-		fh_request_bufs[i] == NULL;
+		fh_request_bufs[i] = NULL;
 	}
         fh_request_freehead = NULL;
         fh_request_bufidx = 0;