BerkeleyLab/gasnet

Add selected C99 features to GASNet coding standards and update compliance
Bug 3296: GASNet (library and headers) now require the C compiler to provide a
short list of widely-implemented C99 features, for general use throughout our
implementation:

  1. C++/C99-style // comments
  2. Mid-block declarations (mixing declarations and code)
  3. Conditional expression declarations, only in for loop headers,
     ie: for (int i=...) { ..i.. }

In addition, we require __VA_ARGS__ support in the $CC used to build libgasnet
(but NOT clients who include gasnet.h).

* Update README with the new C99-related coding standards.

* Add a configure check to confirm the C compiler has the required support to
  build libgasnet.

* Add a configure check for optional __VA_ARGS__ support in the C++ compiler
  (added in C++11).

* The "automatic argument counting" versions of the AM initiation functions
  gasnetex_AM{Request,Reply}{Short,Medium,Long}() are now only available when
  gasnet.h is preprocessed in a compiler where we can detect __VA_ARGS__ support
  (or when forced via -DGASNETI_FORCE_VA_ARG=1).

* Remove configure checks for prohibited features HAVE_CONSTRUCTOR_EXPR,
HAVE_NONCONST_STRUCT_INIT and the only two (mostly pointless) uses.

other/firehose/firehose_internal.h
@@ -176,14 +176,10 @@ extern int	*fhc_RemoteVictimFifoBuckets;
     uintptr_t     addr;
     uintptr_t     node; /* Might instead hold len in region case */
   }	  fh_key_t;
-  #if defined(HAVE_CONSTRUCTOR_EXPR)
-    #define FH_KEYMAKE(addr,node) ((fh_key_t){(addr),(node)})
-  #else
-    GASNETI_ALWAYS_INLINE(fh_keymake)
-    fh_key_t fh_keymake(uintptr_t addr, gasnetex_rank_t node)
-    { fh_key_t key; key.addr = addr; key.node = node; return key; }
-    #define FH_KEYMAKE(addr,node) fh_keymake(addr,node)
-  #endif
+  GASNETI_ALWAYS_INLINE(fh_keymake)
+  fh_key_t fh_keymake(uintptr_t addr, gasnetex_rank_t node)
+  { fh_key_t key; key.addr = addr; key.node = node; return key; }
+  #define FH_KEYMAKE(addr,node) fh_keymake(addr,node)
   #define FH_KEY_EQ(x,y)   (((x).addr == (y).addr) && ((x).node == ((y).node)))
   #define FH_KEY2INT(x)    ((intptr_t)(x).addr ^ (intptr_t)(x).node)
 

evidence/axiom-evi-gasnet

other/firehose/firehose_internal.h
@@ -176,14 +176,10 @@ extern int	*fhc_RemoteVictimFifoBuckets;
     uintptr_t     addr;
     uintptr_t     node; /* Might instead hold len in region case */
   }	  fh_key_t;
-  #if defined(HAVE_CONSTRUCTOR_EXPR)
-    #define FH_KEYMAKE(addr,node) ((fh_key_t){(addr),(node)})
-  #else
-    GASNETI_ALWAYS_INLINE(fh_keymake)
-    fh_key_t fh_keymake(uintptr_t addr, gasnet_node_t node)
-    { fh_key_t key; key.addr = addr; key.node = node; return key; }
-    #define FH_KEYMAKE(addr,node) fh_keymake(addr,node)
-  #endif
+  GASNETI_ALWAYS_INLINE(fh_keymake)
+  fh_key_t fh_keymake(uintptr_t addr, gasnet_node_t node)
+  { fh_key_t key; key.addr = addr; key.node = node; return key; }
+  #define FH_KEYMAKE(addr,node) fh_keymake(addr,node)
   #define FH_KEY_EQ(x,y)   (((x).addr == (y).addr) && ((x).node == ((y).node)))
   #define FH_KEY2INT(x)    ((intptr_t)(x).addr ^ (intptr_t)(x).node)
 