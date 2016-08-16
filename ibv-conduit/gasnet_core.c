e2a023233ed091d6a74082f94a7d11a6f3fb561b
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-08-15 18:06:44 -0700

    ibv: support automatic use of port's active mtu

--- ibv-conduit/gasnet_core.c
+++ ibv-conduit/gasnet_core.c
@@ -820,12 +820,13 @@ static void gasnetc_init_pin_info(int first_local, int num_local) {
 #if GASNET_TRACE
 static const char *mtu_to_str(enum ibv_mtu mtu) {
   switch (mtu) {
+  default          : return "unknown";
+  case 0           : return "active_mtu";
   case IBV_MTU_256 : return "256";
   case IBV_MTU_512 : return "512";
   case IBV_MTU_1024: return "1024";
   case IBV_MTU_2048: return "2048";
   case IBV_MTU_4096: return "4096";
-  default                  : return "unknown";
   }
 }
 #endif
@@ -853,7 +854,7 @@ static int gasnetc_load_settings(void) {
       program_var = _tmp;                                                            \
     } while (0)
   
-  GASNETC_ENVINT(i, GASNET_MAX_MTU, 0, 0, 1);
+  GASNETC_ENVINT(i, GASNET_MAX_MTU, 0, -1, 1);
   switch (i) {
     default: fprintf(stderr,
                      "WARNING: ignoring invalid GASNET_MAX_MTU value %d.\n",
@@ -872,6 +873,8 @@ static int gasnetc_load_settings(void) {
   case 2048: gasnetc_max_mtu = IBV_MTU_2048;
              break;
   case 4096: gasnetc_max_mtu = IBV_MTU_4096;
+             break;
+  case   -1: gasnetc_max_mtu = 0; /* Use port's active_mtu */
              break;
   }
 
