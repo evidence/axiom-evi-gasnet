evidence/axiom-evi-gasnet

commit b8b2d40b826a51c21dab20a882937671c11f83d7

amxtests cleanups
* Remove some left-over obsolete UETH and WIN32 code

* apputils getCurrentTimeMicrosec() uses gasnet_tools when avail

* Fix testam environment check

* disable check-exports for amudp

other/amxtests/apputils.c
@@ -4,9 +4,13 @@
  */
 
 #include "apputils.h"
-#include <time.h>
+#if HAVE_GASNET_TOOLS
+  #include <gasnet_tools.h>
+#else
+  #include <time.h>
+  #include <sys/time.h>
+#endif
 #include <unistd.h>
-#include <sys/time.h>
 #include <signal.h>
 #include <stdlib.h>
 
@@ -92,27 +96,11 @@ void printGlobalStats(void) {
 
 }
 /* ------------------------------------------------------------------------------------ */
-#ifndef UETH
-#ifdef WIN32
+#if HAVE_GASNET_TOOLS
   int64_t getCurrentTimeMicrosec(void) {
-    static int status = -1;
-    static double multiplier;
-    if (status == -1) { /*  first time run */
-      LARGE_INTEGER freq;
-      if (!QueryPerformanceFrequency(&freq)) status = 0; /*  don't have high-perf counter */
-      else {
-        multiplier = 1000000 / (double)freq.QuadPart;
-        status = 1;
-      }
-    }
-    if (status) { /*  we have a high-performance counter */
-      LARGE_INTEGER count;
-      QueryPerformanceCounter(&count);
-      return (int64_t)(multiplier * count.QuadPart);
-    } else { /*  no high-performance counter */
-      /*  this is a millisecond-granularity timer that wraps every 50 days */
-      return (GetTickCount() * 1000);
-    }
+    gasnett_tick_t now = gasnett_ticks_now();
+    int64_t retval = gasnett_ticks_to_ns(now)/1000;
+    return retval;
   }
 #else
   int64_t getCurrentTimeMicrosec(void) {
@@ -123,7 +111,6 @@ void printGlobalStats(void) {
     retval = ((int64_t)tv.tv_sec) * 1000000 + tv.tv_usec;
     return retval;
   }
-#endif
 #endif
 /* ------------------------------------------------------------------------------------ */
 extern void outputTimerStats(void) {
@@ -152,7 +139,9 @@ extern void outputTimerStats(void) {
 }
 /* ------------------------------------------------------------------------------------ */
 #define MAKEDWORD(hi,lo) ((((uint64_t)(hi)) << 32) | (((uint64_t)(lo)) & 0xFFFFFFFF))
+#undef  HIWORD /* prevent a conflict on cygwin */
 #define HIWORD(arg)      ((uint32_t)(((uint64_t)(uintptr_t)(arg)) >> 32))
+#undef  LOWORD /* prevent a conflict on cygwin */
 #define LOWORD(arg)      ((uint32_t)((uint64_t)(uintptr_t)(arg) & 0xFFFFFFFF))
 
 /*  synchronous gets and puts */
@@ -279,7 +268,6 @@ void writeSync(void) {
 }
 /* ------------------------------------------------------------------------------------ */
 void free_resource_handler(int sig) {
-  #if !PLATFORM_OS_MSWINDOWS
     static int first = 1;
     if (first) {
       /* Avoid recursion if a fatal signal is raised while exiting */
@@ -310,7 +298,6 @@ void free_resource_handler(int sig) {
       }
       first = 0;
     }
-  #endif
     
   sleep(2);
   AMX_SPMDExit(-1);
@@ -333,7 +320,6 @@ void setupUtilHandlers(ep_t activeep, eb_t activeeb) {
   AM_Safe(AM_SetHandler(ep, WRITE_REQ_HANDLER, (amx_handler_fn_t)write_request_handler));
   AM_Safe(AM_SetHandler(ep, WRITE_REP_HANDLER, (amx_handler_fn_t)write_reply_handler));
 
-  #if !PLATFORM_OS_MSWINDOWS
     /* some MPI implementations don't cleanup well and leave orphaned nodes
      * if we allow a node to crash without shutting down properly 
      */
@@ -345,6 +331,5 @@ void setupUtilHandlers(ep_t activeep, eb_t activeeb) {
     signal (SIGFPE,  free_resource_handler);
     signal (SIGSEGV, free_resource_handler);
     signal (SIGBUS, free_resource_handler);
-  #endif
 }
 /* ------------------------------------------------------------------------------------ */

other/amxtests/apputils.h
@@ -11,10 +11,6 @@
 #endif
 
 #include <errno.h>
-#ifdef WIN32
-  #include <windows.h>  
-  #define sleep(x) Sleep(1000*x)
-#endif
 
 #if defined(AMUDP)
   #include <amudp.h>
@@ -122,11 +118,7 @@ void setupUtilHandlers(ep_t activeep, eb_t activeeb);
 void printGlobalStats(void);
 
 
-#ifdef UETH
-  #define getCurrentTimeMicrosec() ueth_getustime()
-#else
-  extern int64_t getCurrentTimeMicrosec(void);
-#endif
+extern int64_t getCurrentTimeMicrosec(void);
 extern void outputTimerStats(void);
 
 #define TEST_32BIT_ONLY() do {                                         \

other/amxtests/testam.c
@@ -15,11 +15,17 @@ int main(int argc, char **argv) {
   int iters=0, polling = 1, i;
 
 #if defined(AMUDP)
-  putenv((char*)"A=A");
-  putenv((char*)"B=B");
-  putenv((char*)"C=C");
-  putenv((char*)"ABC=ABC");
-  putenv((char*)"AReallyLongEnvironmentName=A Really Long Environment Value");
+  const char *envvars[][3] = {
+    #define DEFVAR(key,val) { key, val, key "=" val },
+    DEFVAR("A","A")
+    DEFVAR("B","B")
+    DEFVAR("C","C")
+    DEFVAR("ABC","ABC")
+    DEFVAR("AReallyLongEnvironmentName","A Really Long Environment Value")
+  };
+  for (i=0; i < sizeof(envvars)/sizeof(envvars[0]); i++) {
+    putenv((char*)(envvars[i][2]));
+  }
 #endif
 
   TEST_STARTUP(argc, argv, networkpid, eb, ep, 1, 2, "iters (Poll/Block)");
@@ -76,16 +82,14 @@ int main(int argc, char **argv) {
   }
 
 #if defined(AMUDP)
-  if (strcmp(AMX_SPMDgetenvMaster("A"),"A")) 
-    FATALERR("Environment value mismatch on P%i\n", MYPROC);
-  if (strcmp(AMX_SPMDgetenvMaster("B"),"B")) 
-    FATALERR("Environment value mismatch on P%i\n", MYPROC);
-  if (strcmp(AMX_SPMDgetenvMaster("C"),"C"))
-    FATALERR("Environment value mismatch on P%i\n", MYPROC);
-  if (strcmp(AMX_SPMDgetenvMaster("ABC"),"ABC"))
-    FATALERR("Environment value mismatch on P%i\n", MYPROC);
-  if (strcmp(AMX_SPMDgetenvMaster("AReallyLongEnvironmentName"),"A Really Long Environment Value"))
-    FATALERR("Environment value mismatch on P%i\n", MYPROC);
+  for (i=0; i < sizeof(envvars)/sizeof(envvars[0]); i++) {
+    const char *key = envvars[i][0];
+    const char *val = envvars[i][1];
+    const char *actual = AMX_SPMDgetenvMaster(key);
+    if (!actual) actual = "<undef>";
+    if (strcmp(val, actual))
+      printf("P%i: ERROR Environment value mismatch: %s='%s'\n", MYPROC, key, actual);
+  }
 #endif
 
   printf("Slave %i done.\n", MYPROC);

tests/upcr-harness/ammpi/harness.conf
@@ -27,6 +27,7 @@ TestName:	tests
 
 TestName:	distcheck
 AppArgs:        100 P
+TimeLimit:      $DEFAULT$
 RunCmd:		../../../mpi-conduit/contrib/gasnetrun_mpi -np %N %P %A
 WarningFilter:  WARNING: Using Makefile.generic.*?
 PassExpr: Slave 0 done.

tests/upcr-harness/amudp/harness.conf
@@ -18,7 +18,7 @@ END_DEFAULT_CONFIG
 # Tests to compile, but not run
 
 TestName:	check-exports
-RequireFeature: gasnet_has_udp,cc_gnu # Other compilers have too many odd symbol manglings
+RequireFeature: false # C++ has too many odd symbol manglings
 
 TestName:	amxdist
 PassExpr: 	ready for distribution
@@ -27,6 +27,8 @@ TestName:	tests
 
 TestName:	distcheck
 AppArgs:        100 P
+AppEnv: 	A=A B=B C=C ABC=ABC AReallyLongEnvironmentName="A Really Long Environment Value"
+TimeLimit:      $DEFAULT$
 RunCmd:		./amudprun-sa -np %N %P %A
 WarningFilter:  WARNING: Using Makefile.generic.*?
 PassExpr: Slave 0 done.
@@ -74,6 +76,7 @@ BenchmarkResult: Slave.*?throughput:\s*(\S+)\s*(KB/sec)
 
 TestName: testam
 AppArgs: 100 P
+AppEnv: A=A B=B C=C ABC=ABC AReallyLongEnvironmentName="A Really Long Environment Value"
 PassExpr: Slave 0 done.
 
 TestName: testbounce

commit d09000196dc98fb4fac3848a13bd687bfc2d1add

fixup: amxtests cleanups
b8b2d40 caused a missing header regression on apputils.c, for some
OS implementations of select().

other/amxtests/apputils.c
@@ -11,6 +11,7 @@
   #include <sys/time.h>
 #endif
 #include <unistd.h>
+#include <sys/select.h>
 #include <signal.h>
 #include <stdlib.h>
 